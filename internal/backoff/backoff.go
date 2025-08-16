d27595516e2104ab5f1d425f673533f7668c9cc3
Miguel Campos <miguel.campos@p0.dev>
2025-08-16 00:15:45 -0700

    jittered backoff

--- internal/backoff/backoff.go
+++ internal/backoff/backoff.go
@@ -3,6 +3,7 @@ package backoff
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"time"
 )
 
@@ -32,6 +33,19 @@ func (b *Backoff) Next() time.Duration {
 	
 	duration := time.Duration(float64(b.startDuration) * math.Pow(2, float64(b.count-1)))
 	
+	if duration > b.maxDuration {
+		duration = b.maxDuration
+	}
+	
+	// Add jitter: ±25% of the duration to prevent thundering herd
+	jitterRange := float64(duration) * 0.25
+	jitter := time.Duration(rand.Float64()*jitterRange*2 - jitterRange)
+	duration += jitter
+	
+	// Ensure we don't go below 0 or above maxDuration
+	if duration < 0 {
+		duration = b.startDuration
+	}
 	if duration > b.maxDuration {
 		duration = b.maxDuration
 	}
